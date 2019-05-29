d75f2f08d5bb65eccf8ff93dfc7dbcc06a43ab64
Nicolai Davies <Nicolai.d@hotmail.com>
2019-05-29 21:27:45 +0200

    Generation and hint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import hex from 'string-hex';
 
 @Component({
@@ -6,16 +6,19 @@ import hex from 'string-hex';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hex-challenge';
 
   input: string[] = [];
 
-  key = 'HELLO';
-
+  key: string;
   keys = ['A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   correct: boolean = null;
+
+  ngOnInit() {
+    this.key = this.randomString();
+  }
 
   add(letter: string) {
     this.input.push(letter);
@@ -34,6 +37,24 @@ export class AppComponent {
   reset() {
     this.correct = null;
     this.input = [];
+    this.key = this.randomString();
   }
+
+
+  hint() {
+    alert('Starts at 41');
+  }
+
+
+  randomString() {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 4; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
 
 }

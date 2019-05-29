4e36eb45c6517ff8b702e5e842a6dd3b5bdb1b5b
Nicolai Davies <Nicolai.d@hotmail.com>
2019-05-29 21:21:04 +0200

    POC working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import hex from 'string-hex';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hex-challenge';
+
+  input: string[] = [];
+
+  key = 'HELLO';
+
+  keys = ['A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  correct: boolean = null;
+
+  add(letter: string) {
+    this.input.push(letter);
+  }
+
+  delete() {
+    this.input.pop();
+  }
+
+  submit() {
+    const hexValue = hex(this.key) as string;
+    const stringValue = this.input.join('');
+    this.correct =  hexValue.toLowerCase() === stringValue.toLowerCase();
+  }
+
+  reset() {
+    this.correct = null;
+    this.input = [];
+  }
+
 }
